1cc721517565cb27f1408a275e4d04b406960185
marco <cassolamrc@gmail.com>
2019-07-20 14:47:42 +0200

    Edited activity code
    Added login button action.
    Added setPermission method fro fb SDK,
    (https://developers.facebook.com/docs/reference/androidsdk/current/facebook/com/facebook/login/widget/loginbutton.html/)
    Added Callback method (with registerCallback function).
    Added OnActivityResult (@overrride)

--- app/src/main/java/com/example/socialloginoauth2/MainActivity.java
+++ app/src/main/java/com/example/socialloginoauth2/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.socialloginoauth2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // add login button:
+        LoginButton fbLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        fbLoginButton.setPermissions("email");
+        // add callback:
+        callbackManager = CallbackManager.Factory.create();
+
+        // login manager callback:
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+    // on result:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
