95b3dcc770145663fd43fff6a8c59e0896afe47c
marco <cassolamrc@gmail.com>
2019-07-30 19:33:36 +0200

    Add Signout
    added signout intent.
    moved signin and signout intents to specific methods.
    Added button to activity (logout) and listener to invoke
    logout (and login again, after logout).

--- app/src/main/java/com/example/socialloginoauth2/MainActivity.java
+++ app/src/main/java/com/example/socialloginoauth2/MainActivity.java
@@ -2,33 +2,51 @@ package com.example.socialloginoauth2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int RC_SIGN_IN = 9001;
+    Button logout;
+    TextView username;
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.PhoneBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build(),
+            new AuthUI.IdpConfig.FacebookBuilder().build(),
+            new AuthUI.IdpConfig.TwitterBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Choose authentication providers
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.TwitterBuilder().build());
 
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(this);
+        username = findViewById(R.id.accountName);
+        username.setText("");
+
+        startLoginUI();
+    }
+
+    // start sign-in UI:
+    private void startLoginUI() {
         // Create and launch sign-in intent
         startActivityForResult(
                 AuthUI.getInstance()
@@ -37,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -48,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                username.setText(user.getDisplayName());
+
                 // ...
             } else {
                 // Sign in failed. If response is null the user canceled the
@@ -56,5 +78,23 @@ public class MainActivity extends AppCompatActivity {
                 // ...
             }
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        signout();
+        username.setText("");
+        startLoginUI();
+    }
+
+    private void signout() {
+        // signout intent:
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
     }
 }
